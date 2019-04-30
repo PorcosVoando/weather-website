c868e6f950927517c203855dfdd2af8c2064470d
MARTINS Lucas G <lucas.martins@hexagon.com>
2019-04-30 09:35:44 -0300

    Add Temperature High and Low to Forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = function(latitude,longitude,callback){
         } else if(body.error){
             callback('Unable to find location!',undefined)
         } else {
-            callback(undefined,body.daily.data[0].summary + ' its currently: ' + body.currently.temperature + ' °C')
+            callback(undefined,body.daily.data[0].summary + ' its currently: ' + body.currently.temperature + ' °C. There is a ' + body.currently.precipProbability + '% of raining today. The temperature High its: ' + body.daily.data[0].temperatureMax + ' °C, and the temperature low its: ' + body.daily.data[0].temperatureMin + ' °C.' )
         }
     })
 }
